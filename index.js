9e0f32e27622ab716739bcb3505b479a97dc0906
nevace <liam.neville89@gmail.com>
2016-07-01 13:49:08 +0100

    added QUERY

--- index.js
+++ index.js
@@ -12,43 +12,44 @@ class Deputy {
     this.token = token;
   }
 
-  call(method, path, body, params) {
-    const fullPath = this.url + deputyApiUrl + path;
-    let postBody;
-    let contentType;
-    params = params || {};
+  call(method, path, body, params, meta) {
+    const fullPath = this.url + deputyApiUrl + path + meta;
+    params = params || '';
 
-    if (method && method !== 'GET' && body) {
-      throw new Error('In development');
+    if (method === 'POST') {
+      if (meta === '/QUERY') {
+        return this._getAll(method, fullPath, body, params);
+      }
       //return this._post(method, fullPath, params, postBody, contentType, path, body);
     } else {
-      return this._getAll(method, fullPath, params);
+      throw new Error('In development');
     }
   }
 
-  _getAll(method, fullPath, params) {
+  _getAll(method, fullPath, body, params) {
     return co(function*() {
       let lastCount = 0;
       let totalCount = 0;
       let result;
       let fullResult = [];
-      params.start = totalCount;
+      body.start = totalCount;
 
       while (true) {
-        result = yield this._request(method, fullPath, params);
+        result = yield this._request(method, fullPath, params, body);
         fullResult.push(...result);
         lastCount = result.length;
 
-        if (lastCount < 500) break;
+        if (!lastCount) break;
 
         totalCount += lastCount;
-        params.start = totalCount;
+        body.start = totalCount;
+        console.log(body)
       }
       return fullResult;
     }.bind(this));
   }
 
-  _request(method, fullPath, params, postBody, batch) {
+  _request(method, fullPath, params, body, batch) {
     let batchLog = batch ? `batch ${batch.batchNo} of ${batch.batchCount}` : '';
     batchLog += batch && batch.subBatchNo ? `, sub-batch ${batch.subBatchNo} of ${batch.subBatchCount}` : '';
     const log = `${method} ${fullPath}${params} ${batchLog}`;
@@ -62,12 +63,16 @@ class Deputy {
         'Content-Type': 'application/json',
         'Unixtime-Convert-Format': 'C'
       },
-      qs: params
+      qs: params,
+      json: true,
+      body: body
     };
 
     return new Promise((resolve, reject) => {
       request(args, function(err, response, body) {
         if (err) return reject(err);
+        console.log(response)
+          // console.log(response)
         body = (typeof body === 'string') ? JSON.parse(body) : body;
         resolve(body);
       });
